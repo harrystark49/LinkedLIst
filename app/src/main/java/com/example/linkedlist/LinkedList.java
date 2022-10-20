0a165617e2566de58afca7992af6ad01df5e9832
Harrystark49 <harikrishna.aravakantha@xcubelabs.com>
2022-10-20 15:48:23 +0530

    LinkedList

---
+++ app/src/main/java/com/example/linkedlist/LinkedList.java
@@ -0,0 +1,53 @@
+package com.example.linkedlist;
+
+import android.util.Log;
+
+public class LinkedList<T> {
+    Node head;
+    class Node<T>{
+        T data;
+        Node next;
+        Node(T data){
+            this.data=data;
+            next=null;
+        }
+    }
+
+    void insert(T data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+        }else{
+            Node temp=head;
+            while(temp.next != null){
+                temp=temp.next;
+            }
+            temp.next=newNode;
+        }
+    }
+
+    void delete() {
+        Node temp = head;
+        if (temp != null) {
+            while (temp.next != null) {
+                if(temp.next.next==null) {
+                    break;
+                }
+                temp=temp.next;
+            }
+                temp.next = null;
+        }
+    }
+
+    void print() {
+        Node temp = head;
+        if (temp != null) {
+            while (temp.next != null) {
+                Log.d("print_linkedList", String.valueOf(temp.data));
+                temp = temp.next;
+            }
+            Log.d("print_linkedList", String.valueOf(temp.data));
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/linkedlist/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.linkedlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var ll=LinkedList<String>()
+
+        ll.apply {
+            insert("harry")
+            insert("stark")
+            insert("chinnu")
+            insert("maam")
+        }
+        ll.print()
+        ll.delete()
+        ll.print()
+    }
+}
